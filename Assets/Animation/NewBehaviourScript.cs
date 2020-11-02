1cd81d82bf24b2bed39ea54ff811953717e31675
LouiseDoorr <ldni18@student.aau.dk>
2020-11-02 10:23:00 +0100

    add Animation

---
+++ Assets/Animation/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Animator anim;
+    int taskHash = Animator.StringToHash("taskOn");
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // anim.SetBool(taskHash, false);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(taskHash);
+        }
+        // AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+
+    }
+}
